366b741addde871ff8b6466e860cb733e245a393
xing.research <xing.research@gmail.com>
2014-10-20 17:22:29 +0000

    science run

--- pscf/purif.c
+++ pscf/purif.c
@@ -145,7 +145,7 @@ static void config_purif (purif_t * purif, int purif_offload)
     memsize = (2.0 * MAX_DIIS + 14.0) * meshsize * sizeof (double);
     if (myrow == 0 && mycol == 0 && mygrd == 0)
     {
-        printf ("  CPU uses %.3lf MB\n", memsize / 1024.0 / 1024.0);
+        printf ("  CPU uses %.3f MB\n", memsize / 1024.0 / 1024.0);
     }
 }
 
@@ -639,3 +639,178 @@ void compute_diis (PFock_t pfock, purif_t * purif,
 
     MPI_Barrier(MPI_COMM_WORLD);
 }
+
+
+#if 1
+static void peig(int ga_A, int ga_B, int n, int nprow, int npcol, double *eval)
+{
+    int myrank;
+    int ictxt;
+    int myrow;
+    int mycol;
+    int nn;
+    int mm;
+    int izero = 0;
+    int descA[9];
+    int descZ[9];
+    int info;
+    int lo[2];
+    int hi[2];
+    int ld;
+    int ione = 1;
+#ifdef GA_NB
+    ga_nbhdl_t nbnb;
+#endif
+
+    // init blacs
+    int nb = MIN(n / nprow, n / npcol);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    Cblacs_pinfo(&nn, &mm);
+    Cblacs_get(-1, 0, &ictxt);
+    Cblacs_gridinit(&ictxt, "Row", nprow, npcol);
+    Cblacs_gridinfo(ictxt, &nprow, &npcol, &myrow, &mycol);
+
+    // init matrices
+    int nrows = numroc_(&n, &nb, &myrow, &izero, &nprow);
+    int ncols = numroc_(&n, &nb, &mycol, &izero, &npcol);
+    int itemp = nrows > 1 ? nrows : 1;
+    descinit_(descA, &n, &n, &nb, &nb, &izero, &izero, &ictxt, &itemp, &info);
+    descinit_(descZ, &n, &n, &nb, &nb, &izero, &izero, &ictxt, &itemp, &info);
+    int blocksize = nrows * ncols;
+    double *A = (double *)mkl_malloc(blocksize * sizeof (double), 64);
+    assert (A != NULL);
+    double *Z = (double *)mkl_malloc(blocksize * sizeof (double), 64);
+    assert (Z != NULL);
+
+    // distribute source matrix
+    for (int i = 1; i <= nrows; i += nb) {
+        lo[0] = indxl2g_(&i, &nb, &myrow, &izero, &nprow) - 1;
+        hi[0] = lo[0] + nb - 1;
+        hi[0] = hi[0] >= n ? n - 1 : hi[0];
+        for (int j = 1; j <= ncols; j += nb) {
+            lo[1] = indxl2g_(&j, &nb, &mycol, &izero, &npcol) - 1;
+            hi[1] = lo[1] + nb - 1;
+            hi[1] = hi[1] >= n ? n - 1 : hi[1];
+            ld = ncols;
+#ifdef GA_NB
+            NGA_NbGet(ga_A, lo, hi, &(Z[(i - 1) * ncols + j - 1]), &ld, &nbnb);
+#else
+            NGA_Get(ga_A, lo, hi, &(Z[(i - 1) * ncols + j - 1]), &ld);
+#endif
+        }
+        /* Jeff: Location of NGA_NbWait for flow-control. */
+    }
+#ifdef GA_NB
+    /* Jeff: If one sees flow-control problems with too many
+     *       outstanding NbGet operations, then move this call
+     *       to the location noted above. */
+    NGA_NbWait(&nbnb);
+#endif
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            A[j * nrows + i] = Z[i * ncols + j];
+        }
+    }
+
+    double t1 = MPI_Wtime();
+    // inquire working space
+    double *work = (double *)mkl_malloc(2 * sizeof (double), 64);
+    assert (work != NULL);
+    int lwork = -1;
+#if 0
+    pdsyev ("V", "U", &n, A, &ione, &ione, descA,
+            eval, Z, &ione, &ione, descZ, work, &lwork, &info);
+#else
+    int liwork = -1;
+    int *iwork = (int *)mkl_malloc(2 * sizeof (int), 64);
+    assert(iwork != NULL);
+    pdsyevd("V", "U", &n, A, &ione, &ione, descA,
+            eval, Z, &ione, &ione, descZ,
+            work, &lwork, iwork, &liwork, &info);    
+#endif
+
+    // compute eigenvalues and eigenvectors
+    lwork = (int)work[0] * 2;
+    mkl_free(work);
+    work = (double *)mkl_malloc(lwork * sizeof (double), 64);
+    assert(work != NULL);
+#if 0
+    pdsyev ("V", "U", &n, A, &ione, &ione, descA,
+            eval, Z, &ione, &ione, descZ, work, &lwork, &info);
+#else
+    liwork = (int)iwork[0];
+    mkl_free(iwork);
+    iwork = (int *)mkl_malloc(liwork * sizeof (int), 64);
+    assert(iwork != NULL);
+    pdsyevd("V", "U", &n, A, &ione, &ione, descA,
+            eval, Z, &ione, &ione, descZ,
+            work, &lwork, iwork, &liwork, &info); 
+#endif
+    double t2 = MPI_Wtime();
+    if (myrank == 0) {
+        printf("  pdsyev_ takes %.3f secs\n", t2 - t1);
+    }
+
+    // store desination matrix
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            A[i * ncols + j] = Z[j * nrows + i];
+        }
+    }
+    for (int i = 1; i <= nrows; i += nb) {
+        lo[0] = indxl2g_ (&i, &nb, &myrow, &izero, &nprow) - 1;
+        hi[0] = lo[0] + nb - 1;
+        hi[0] = hi[0] >= n ? n - 1 : hi[0];
+        for (int j = 1; j <= ncols; j += nb) {
+            lo[1] = indxl2g_ (&j, &nb, &mycol, &izero, &npcol) - 1;
+            hi[1] = lo[1] + nb - 1;
+            hi[1] = hi[1] >= n ? n - 1 : hi[1];
+            ld = ncols;
+#ifdef GA_NB
+            NGA_NbPut(ga_B, lo, hi, &(A[(i - 1) * ncols + j - 1]), &ld, &nbnb);
+#else
+            NGA_Put(ga_B, lo, hi, &(A[(i - 1) * ncols + j - 1]), &ld);
+#endif
+        }
+        /* Jeff: Location of NGA_NbWait for flow-control. */
+    }
+#ifdef GA_NB
+    /* Jeff: If one sees flow-control problems with too many
+     *       outstanding NbPut operations, then move this call
+     *       to the location noted above. */
+    NGA_NbWait(&nbnb);
+#endif
+    GA_Sync();
+
+    mkl_free(A);
+    mkl_free(Z);
+    mkl_free(work);
+
+    Cblacs_gridexit(ictxt);
+}
+
+
+void compute_eigensolve(int ga_tmp, purif_t * purif,
+                        double *F_block, int nprow, int npcol)
+{
+    int nbf = purif->nbf;
+    if (purif->runpurif == 1) {
+        int lo[2];
+        int hi[2];
+        int ld;
+        lo[0] = purif->srow_purif;
+        hi[0] = purif->srow_purif + purif->nrows_purif - 1;
+        lo[1] = purif->scol_purif;
+        hi[1] = purif->scol_purif + purif->ncols_purif - 1;
+        ld = purif->ldx;
+        NGA_Put(ga_tmp, lo, hi, F_block, &ld);
+    }
+
+    double *eval = (double *)mkl_malloc(nbf * sizeof (double), 64);
+    assert(eval != NULL);
+    peig(ga_tmp, ga_tmp, nbf, nprow, npcol, eval);
+
+    mkl_free(eval);
+}
+
+#endif
